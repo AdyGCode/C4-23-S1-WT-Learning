8c2ece13ac7b1b53cf623a964422022ab0c87497
Adrian Gould <adrian.gould@nmtafe.wa.edu.au>
2023-05-05 08:48:34 +0800

    feat: Session 13
    - dom-00.{html|js} added for demonstration

---
+++ Session-13/assets/js/dom-00.js
@@ -0,0 +1,11 @@
+let dom = document;
+
+const outputZone=dom.getElementById("outputZone")
+let nameEntry = dom.getElementById("name")
+
+function formCallback(event){
+  // Do Something Here
+}
+
+
+nameEntry.addEventListener("whatListeningFor", callbackFunction)
